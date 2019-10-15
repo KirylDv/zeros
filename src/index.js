2b3da922ed7f6209206c25e6b7e3eb4733586602
KirylDv <39598590+KirylDv@users.noreply.github.com>
2019-10-15 20:39:04 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+function factorial(num){
+  if (num <= 1n)
+    return 1n;
+  return factorial(num-1n) * num;
+}
+
+function double_factorial(num){
+  if (num <= 1n)
+    return 1n;
+  return double_factorial(num-2n) * num;
+}
+
 module.exports = function zeros(expression) {
-  // your solution
+  let a = expression.split("*");
+  let resultNum = 1n;
+  a.forEach(elem => {
+    if (elem[elem.length-2] === "!")
+      resultNum *= double_factorial( BigInt(elem.slice(0, -2)) );
+    else
+      resultNum *= factorial( BigInt(elem.slice(0, -1)) );
+  });
+  a = String(resultNum);
+  resultNum = 0;
+  console.log(a);
+  for (let i = a.length-1; i > 0; i--) {
+    if(a[i] !== "0")
+      break;
+    resultNum++;
+  }
+  return resultNum;
 }
